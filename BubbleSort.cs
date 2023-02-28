ce7fe888a54505469235d6b5fe80803e42299a83
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-02-28 11:18:16 +0530

    BubbleSort

--- BubbleSort.cs
+++ BubbleSort.cs
@@ -8,29 +8,7 @@ namespace Algorithm
 {
     public class BubbleSort
     {
-        /*public void DisplayNumber()
-        {
-            int[] arr = { 78, 55, 45, 98, 13 };
-            int temp;
-            for(int j = 0; j <= arr.Length-2; j++)
-            {
-                for(int i = 0; i<= arr.Length-2; i++)
-                {
-                    if (arr[i] > arr[i+1])
-                    {
-                        temp = arr[i];
-                        arr[i + 1] = arr[i];
-                        arr[i] = temp;
-                    }
-                }
-
-            }
-            Console.WriteLine("Sorted");
-            foreach(int p in arr)
-            {
-                Console.WriteLine(p);
-            }
-        }*/
+        
         public void Bubble()
         {
             BubbleSort bubbleSort = new BubbleSort();
