c77267b48892e514165e150e416c332f04cbe7bb
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-02-28 10:15:04 +0530

    ADDED:Permutations Of String

---
+++ PermutationsOfString.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class PermutationsOfString
+    {
+        public static String Swap(String Strings, int leftIndex, int rightIndex)
+        {
+            char[] tempString = Strings.ToCharArray();
+
+            char temp = tempString[leftIndex];
+            tempString[leftIndex] = tempString[rightIndex];
+            tempString[rightIndex] = temp;
+
+            string SReturn = new string(tempString);
+            return SReturn;
+        }
+        public static void Permute(String Strings, int leftIndex, int rightIndex)
+        {
+            if (leftIndex == rightIndex)
+            {
+                Console.WriteLine(Strings);
+            }
+            
+            else
+            {
+                for (int i = leftIndex; i <= rightIndex; i++)
+                {
+                    Strings = Swap(Strings, leftIndex, i);
+                    Permute(Strings, leftIndex + 1, rightIndex);
+                    Strings = Swap(Strings, leftIndex, i);
+                }
+            }
+        }
+        public void Display()
+        {
+            Console.Write("Enter the string to find all of it's permutations : ");
+            string Strings = Console.ReadLine();
+            Console.WriteLine("Permutations of the string " + Strings + " are :");
+            Permute(Strings, 0, Strings.Length - 1);
+        }
+    }
+
+}
